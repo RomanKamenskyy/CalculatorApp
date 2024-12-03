95e94407d66a328f9c12aa1d11a2726f8adb2eb3
roman <kamensky.ra@gmail.com>
2024-12-03 22:05:41 +0100

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by roman on 03.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
